76a71b47aad0d9654395cde22c64bb3ad223dbea
Scott Hussey <sh8121@att.com>
2018-02-28 11:24:10 -0600

    Update to UCP layout standard
    - Create Makefile for image build
    - Move Dockerfile into images/pegleg
    - Move pegleg module src to src/bin/pegleg
    
    Change-Id: I8fd728888ecfd75fe857da253d6c8cd4fd83f89c

--- tools/pegleg.sh
+++ tools/pegleg.sh
@@ -16,13 +16,6 @@ fi
 
 IMAGE_PEGLEG=${IMAGE_PEGLEG:-quay.io/attcomdev/pegleg:latest}
 
-if [[ -z ${http_proxy} && -z ${https_proxy} ]]
-then
-    docker build -q --rm -t "${IMAGE_PEGLEG}" "${SOURCE_DIR}" > /dev/null
-else
-    docker build -q --rm -t "${IMAGE_PEGLEG}" --build-arg http_proxy=${http_proxy} --build-arg https_proxy=${https_proxy}  "${SOURCE_DIR}" > /dev/null
-fi
-
 docker run --rm -t \
     -v "${WORKSPACE}:/var/pegleg" \
     "${IMAGE_PEGLEG}" \
